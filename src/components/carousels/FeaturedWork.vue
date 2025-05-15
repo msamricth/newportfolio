1eca41b5052d0fb546f4aebdaa6bcf974b216362
msamricth <emm.talarico@gmail.com>
2025-05-15 10:32:51 -0400

    Hotfix

--- src/components/carousels/FeaturedWork.vue
+++ src/components/carousels/FeaturedWork.vue
@@ -71,7 +71,7 @@ import { useMainStore } from '../../stores/main.js'
 import { useModalStore } from '../../stores/modal.js'
 import placeholderJS from '../../utils/placeholder.js'
 import { work as slides } from '../../data/work.js';
-import MainButton from '../Buttons/MainButton.vue'
+import MainButton from '../buttons/MainButton.vue'
 import videoHandler from '../../utils/videoHandler.js';
 gsap.registerPlugin(ScrollTrigger);
 const store = useMainStore()
