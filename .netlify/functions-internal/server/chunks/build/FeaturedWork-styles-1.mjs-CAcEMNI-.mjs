8a530cfe49014efd6faff7b9dd1bce55bc8cd55e
msamricth <emm.talarico@gmail.com>
2025-05-27 19:14:00 -0400

    Backend updates pt2

--- .netlify/functions-internal/server/chunks/build/FeaturedWork-styles-1.mjs-CAcEMNI-.mjs
+++
@@ -1,4 +0,0 @@
-const FeaturedWork_vue_vue_type_style_index_0_scoped_743e9c9d_lang = "@keyframes spin-slow-743e9c9d{0%{transform:rotate(0)}to{transform:rotate(1turn)}}.animate-spin-slow[data-v-743e9c9d]{animation:spin-slow-743e9c9d 4s linear infinite}.gradient-border[data-v-743e9c9d]{-o-border-image:conic-gradient(from 0deg,#00ffd5,#ff00c3,#00ffd5) 1;border-image:conic-gradient(from 0deg,#00ffd5,#ff00c3,#00ffd5) 1}";
-
-export { FeaturedWork_vue_vue_type_style_index_0_scoped_743e9c9d_lang as F };
-//# sourceMappingURL=FeaturedWork-styles-1.mjs-CAcEMNI-.mjs.map
--- .netlify/functions-internal/server/chunks/build/Sections-styles-1.mjs-4lQdGgNM.mjs
+++
@@ -1,4 +0,0 @@
-const Sections_vue_vue_type_style_index_0_scoped_feb4a792_lang = ".icon-btn[data-v-feb4a792],.image[data-v-feb4a792],.tl[data-v-feb4a792]{will-change:transform,opacity}";
-
-export { Sections_vue_vue_type_style_index_0_scoped_feb4a792_lang as S };
-//# sourceMappingURL=Sections-styles-1.mjs-4lQdGgNM.mjs.map
