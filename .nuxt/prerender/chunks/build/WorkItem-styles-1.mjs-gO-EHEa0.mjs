dd58ad50b3abc54b0b56d7ef67feea07806c51c3
msamricth <emm.talarico@gmail.com>
2025-07-01 15:04:22 -0400

    Delete .nuxt directory

--- .nuxt/prerender/chunks/build/WorkItem-styles-1.mjs-gO-EHEa0.mjs
+++
@@ -1,4 +0,0 @@
-const WorkItem_vue_vue_type_style_index_0_scoped_6c881a41_lang = "@keyframes spin-slow-6c881a41{0%{transform:rotate(0)}to{transform:rotate(1turn)}}.animate-spin-slow[data-v-6c881a41]{animation:spin-slow-6c881a41 4s linear infinite}.gradient-border[data-v-6c881a41]{-o-border-image:conic-gradient(from 0deg,#00ffd5,#ff00c3,#00ffd5) 1;border-image:conic-gradient(from 0deg,#00ffd5,#ff00c3,#00ffd5) 1}";
-
-export { WorkItem_vue_vue_type_style_index_0_scoped_6c881a41_lang as W };
-//# sourceMappingURL=WorkItem-styles-1.mjs-gO-EHEa0.mjs.map
